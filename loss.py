ec8a0e6f548e2e3a0aea37a212c501324a2e4071
HowardZhangdqs <zjh@shanghaiit.com>
2025-01-08 14:45:49 +0800

    fix: loss function

--- loss.py
+++ loss.py
@@ -4,11 +4,16 @@ import torch.nn.functional as F
 
 
 class Loss(nn.Module):
-    def __init__(self, device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+    def __init__(self, distribution=[1, 1], device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
         super(Loss, self).__init__()
         self.device = device
+        self.distribution = distribution
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         target = target[:, :2].float().to(pred.device) + torch.rand((target.size(0), 2), device=self.device) * 1e-3
-        loss = F.binary_cross_entropy(F.sigmoid(pred), F.sigmoid(target))
+
+        loss = F.binary_cross_entropy_with_logits(
+            pred, target,
+            pos_weight=torch.tensor(self.distribution).to(self.device)
+        )
         return loss
